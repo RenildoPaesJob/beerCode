d642a55c8ec7f46b861d166efb24f650f3781de8
RenildoPaesJob <renildopaesjoob@gmail.com>
2024-07-29 22:56:19 -0300

    feat: add migrations

---
+++ app/Http/Controllers/SignatureHistoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SignatureHistory;
+use Illuminate\Http\Request;
+
+class SignatureHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SignatureHistory $signatureHistory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SignatureHistory $signatureHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SignatureHistory $signatureHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SignatureHistory $signatureHistory)
+    {
+        //
+    }
+}
