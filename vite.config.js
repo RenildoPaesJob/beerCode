58f946a08f77529fba88f1ffda0902343f70e197
RenildoPaesJob <renildopaesjoob@gmail.com>
2024-07-29 22:29:22 -0300

    feat: install breeze

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-	plugins: [
-		laravel({
-			input: ['resources/css/app.css', 'resources/js/app.js'],
-			refresh: true,
-		}),
-	],
-	watch: {
-		usePolling: true,
-		origin: 'http://localhost:3000'
-	},
-	server: {
-		hmr: {
-			host: 'localhost'
-		},
-		host: true,
-		port: 3000,
-		watch: {
-			usePolling: true
-		}
-	}
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
 });
