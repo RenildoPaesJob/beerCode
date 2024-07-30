5de49eb5dcfb41a25a505eee84ca082755dea59a
RenildoPaesJob <renildopaesjoob@gmail.com>
2024-07-29 22:10:31 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,24 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+	plugins: [
+		laravel({
+			input: ['resources/css/app.css', 'resources/js/app.js'],
+			refresh: true,
+		}),
+	],
+	watch: {
+		usePolling: true,
+		origin: 'http://localhost:3000'
+	},
+	server: {
+		hmr: {
+			host: 'localhost'
+		},
+		host: true,
+		port: 3000,
+		watch: {
+			usePolling: true
+		}
+	}
 });
